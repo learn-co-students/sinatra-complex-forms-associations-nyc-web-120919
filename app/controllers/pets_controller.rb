638fb73e51d1aacbf60daaee1a86f9043cffdf37
flatironschool <danliu277@gmail.com>
2020-01-07 15:12:51 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,29 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @pet = Pet.create(name: params["pet_name"])
 
+    if(params[:owner_id])
+      @owner = Owner.find(params[:owner_id])
+    elsif(params[:owner_name])
+      @owner = Owner.create(name: params[:owner_name])
+    end
+
+    @pet.owner_id = @owner.id
+    @pet.save
+    @owner.pets << @pet
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,6 +37,18 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params[:pet_name])
+
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @owner.pets << @pet
+      @pet.owner_id = @owner.id
+    elsif params[:owner_id]
+      @owner = Owner.find(params[:owner_id])
+      @owner.pets << @pet
+      @pet.owner_id = @owner.id
+    end
 
     redirect to "pets/#{@pet.id}"
   end
